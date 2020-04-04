87ca35c8735af2109deb41c6bde323ec6107e8a2
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-04 03:08:28 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :content, length { min: 100 }
+  validates :content, length { minimu: 100 }
 end
